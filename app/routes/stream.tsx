23a0a05e41b89693cc7e6ba37e325b9b0cf094e4
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2025-06-04 21:31:50 +0200

    Experiments

--- app/routes/stream.tsx
+++ app/routes/stream.tsx
@@ -1,85 +1,30 @@
 import type { Route } from ".react-router/types/app/routes/+types/stream";
-import { Chunk, Effect, Schedule, Scope, Stream } from "effect";
-import { ByteLengthQueuingStrategy } from "node:stream/web";
-
-const createEventStream = (signal: AbortSignal) => {
-  const textEncoder = new TextEncoder();
-  const streamAbortController = new AbortController();
-
-  let readableStreamController: ReadableStreamDefaultController | undefined;
-  let closed = false;
-
-  const close = () => {
-    if (closed) {
-      return;
-    }
-
-    closed = true;
-
-    readableStreamController?.close();
-
-    streamAbortController.abort();
-  };
-
-  const stream = new ReadableStream({
-    cancel: () => {
-      close();
-    },
-    start: (ctrl) => {
-      readableStreamController = ctrl;
-
-      if (signal.aborted) {
-        close();
-
-        return;
-      }
-
-      signal.addEventListener(
-        "abort",
-        () => {
-          close();
-        },
-        { signal: streamAbortController.signal }
-      );
-    },
-  });
-
-  const send = ({ data, event }: { data: string; event: string }) => {
-    if (closed || !readableStreamController) {
-      return;
-    }
-
-    const chunk = `event: ${event}\ndata:${data}\n\n`;
-
-    readableStreamController.enqueue(textEncoder.encode(chunk));
-  };
-
-  return {
-    get closed() {
-      return closed;
-    },
-    complete: close,
-    response: new Response(stream, {
-      headers: {
-        "Cache-Control": "no-cache",
-        Connection: "keep-alive",
-        "Content-Type": "text/event-stream",
-      },
-    }),
-    send,
-  };
-};
+import { faker } from "@faker-js/faker";
+import { Effect, Schedule, Sink, Stream } from "effect";
 
 export const loader = ({ request }: Route.LoaderArgs) => {
   return Effect.runPromise(
     Effect.gen(function* () {
-      const stream = Stream.fromSchedule(Schedule.spaced("100 millis")).pipe(
-        Stream.take(1000)
+      const stream = Stream.fromSchedule(Schedule.spaced("1 millis")).pipe(
+        Stream.take(10_00),
+        Stream.map((index) => ({
+          id: crypto.randomUUID(),
+          firstName: faker.person.firstName(),
+          lastName: faker.person.lastName(),
+          middleName: faker.person.middleName(),
+          bio: faker.person.bio(),
+          sex: faker.person.sex(),
+          index,
+        }))
       );
+
+      Stream.toPull;
 
       const value = yield* stream.pipe(
         Stream.tap(Effect.log),
-        Stream.buffer({ capacity: "unbounded" }),
+        Stream.map(
+          (person) => `event: person\ndata:${JSON.stringify(person)}\n\n`
+        ),
         Stream.toReadableStreamEffect()
       );
 
@@ -90,6 +35,18 @@ export const loader = ({ request }: Route.LoaderArgs) => {
           "Content-Type": "text/event-stream",
         },
       });
-    }).pipe(Effect.scoped)
+    })
   );
 };
+
+if (import.meta.env.SSR) {
+  Effect.gen(function* () {
+    const memoryUsage = process.memoryUsage();
+
+    yield* Effect.log(
+      `Heap used: ${(memoryUsage.heapUsed / 1024 / 1024).toFixed(2)} MB`
+    );
+
+    yield* Effect.log(`RSS: ${(memoryUsage.rss / 1024 / 1024).toFixed(2)} MB`);
+  }).pipe(Effect.schedule(Schedule.spaced("5 seconds")), Effect.runPromise);
+}
